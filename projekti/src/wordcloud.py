a807fbb0f472272b0f350910bda7135bcd79ea3e
Martin Mäkipää <martin.makipaa@gmail.com>
2022-04-12 15:16:01 +0300

    Lisää dokumentaatiota, globaaleja muuttujia lokaaleiksi

--- projekti/src/wordcloud.py
+++ projekti/src/wordcloud.py
@@ -4,14 +4,20 @@ import matplotlib.pyplot as plt
 
 
 class WordCloud:
+    """
+    This class contains the functionalities to create
+    a word cloud. It: 
+    - takes a .txt file as an input & reads it to a str variable
+    - removes punctuation marks and sets upper case char to lower case
+    - splits the string to a list of words
+    - draws a 2d wordcloud from the list of words
+    - saves the plot to the root of this project as sanapilvi.png
+    """
 
     def __init__(self):
         self.text = ""
         self.text_modified = ""
         self.text_list = []
-        self.punctuation = [".", ",", "?", "!", "/"]
-        self.special_punctuation = ["/"]
-        self.coordinate_dict = {}
         self.word_counts = {}
         self.x_array = None
         self.y_array = None
@@ -21,11 +27,11 @@ class WordCloud:
         Reads a txt-file
         """
         fullpath = os.path.join(os.getcwd()[:-9], dir)
-        print("Path:\n", fullpath)
+        print("Reading the data file from the path:\n", fullpath)
         try:
             with open(fullpath, encoding=encode) as file:
                 self.text = ''.join(file.readlines())
-            print("Text data read successfully.")
+            print("\nText data read successfully.")
 
         except Exception as e:
             print("An exception occurred:\n", e)
@@ -36,27 +42,35 @@ class WordCloud:
         - setting all characters lower case
         - removing punctuation marks 
         """
+        print("\nRemoving the punctuation marks...")
+        punctuation = [".", ",", "?", "!", "/"]
+        special_punctuation = "/"
         self.text_modified = self.text.lower()
-        for i in self.punctuation:
-            if i in self.special_punctuation:
+        for i in punctuation:
+            if i == special_punctuation:
                 self.text_modified = self.text_modified.replace(i, " ")
             else:
                 self.text_modified = self.text_modified.replace(i, "")
-        print("Punctuation marks removed")
+        print("Punctuation marks removed.")
 
     def string_to_list(self):
-        self.text_list = list(self.text.split(" "))
+        "Transforms the string to a list of words."
+        print("\nCreating a list of words...")
+        self.text_list = list(self.text_modified.split(" "))
         print("A list of words created from the string.")
 
     def count_words(self):
         """
         Counts the occurrences of each unique word in the list
         """
+        print("\nCounting the frequencies of the words...")
         self.word_counts = {word: 0 for word in set(self.text_list)}
         for word in self.text_list:
             self.word_counts[word] += 1
         self.word_counts = sorted(
             self.word_counts.items(), key=lambda x: x[1], reverse=True)
+        print("Frequencies calculated. The most frequent word is:\n",
+              self.word_counts[0][0], "with", self.word_counts[0][1], "appearances.")
 
     def set_coordinates(self):
         """
@@ -72,6 +86,7 @@ class WordCloud:
         Creates a wordcloud. The size of the words is defined
         by number of their appearnce in the given dataset.
         """
+        print("\nCreating a wordcloud...")
         # Set up colors
         colors = ["#069AF3", "#13EAC9", "#90EE90", "#00FF00"]
         curr_color = ""
@@ -97,3 +112,4 @@ class WordCloud:
         ax.axis("off")
         # Saves the figure
         fig.savefig("sanapilvi.png")
+        print("Wordcloud created and saved successfully.")
